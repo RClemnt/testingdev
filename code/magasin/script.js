e9ed102243fa1f7114fb6927a4c8fb159be1b343
pela37 <p.goupil@tekin.fr>
2024-05-16 17:11:41 +0200

    meilleur panier page magasin

--- code/magasin/script.js
+++ code/magasin/script.js
@@ -49,31 +49,38 @@ function moins(id) {
   document.querySelector(id).innerHTML = nb;
 }
 
-function ajoutPanier(id_qte, id_li, id_span) {
+function ajoutPanier(id_qte, id_tr, id_span) {
   var qte = parseInt(document.querySelector(id_qte).innerHTML);
-  if(qte > 0)
-    document.querySelector(id_li).style.display = "block";
+  if(qte > 0) {
+    document.querySelector(id_tr).style.visibility = "visible";
+    document.querySelector("#panier").style.visibility = "visible";
+  }
   qtePanier[id_span] += qte;
   document.querySelector(id_span).innerHTML = qtePanier[id_span];
 }
 
-function supprProduit(id_li, id_span, nom) {
+function supprProduit(id_tr, id_span, nom) {
   if(window.confirm("Voulez vous vraiment supprimer " + nom + " de votre panier?")) {
     if(window.confirm("En êtes-vous vraiment sûr?")) {
       if(window.confirm("Attention, si vous supprimez " + nom + ", cela n'apparaitra plus dans le panier")) {
         if(window.confirm("Cela supprimera également la quantité que vous avez entrer!")) {
-          setTimeout(regret, 4000, id_li, id_span, nom, qtePanier[id_span]);
+          setTimeout(regret, 4000, id_tr, id_span, nom, qtePanier[id_span]);
           qtePanier[id_span] = 0;
-          document.querySelector(id_li).style.display = "none";
+          document.querySelector(id_tr).style.visibility = "collapse";
+          var val = Object.values(qtePanier)
+          if(val[0] == 0 && val[1] == 0 && val[2] == 0 && val[3] == 0) {
+            document.querySelector("#panier").style.visibility = "collapse";
+          }
         }
       }
     }
   }
 }
 
-function regret(id_li, id_span, nom, qte) {
+function regret(id_tr, id_span, nom, qte) {
   if(window.confirm("Si vous regrettez d'avoir supprimer " + nom + " de votre panier, nous pouvons le remettre. Regrettez-vous votre choix?")) {
     qtePanier[id_span] = qte;
-    document.querySelector(id_li).style.display = "block";
+    document.querySelector(id_tr).style.visibility = "visible";
+    document.querySelector("#panier").style.visibility = "visible";
   }
 }
